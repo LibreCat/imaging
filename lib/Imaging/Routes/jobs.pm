80b07f4a9aead5acf0d8fe8e8fb03fa6a0eb2423
Nicolas Franck <njfranck@ca20c521.ugent.be>
2013-03-21 15:51:55 +0100

    job tracking mediamosa

---
+++ lib/Imaging/Routes/jobs.pm
@@ -0,0 +1,53 @@
+package Imaging::Routes::jobs;
+use Dancer ':syntax';
+use MediaMosa;
+use Catmandu::Sane;
+use Catmandu::Util qw(:is :array);
+use Try::Tiny;
+
+get '/jobs/:asset_id' => sub {
+
+  my $params = params;
+  my(@errors,@messages,@hits);
+  my $response = {errors => \@errors,messages => \@messages,hits => \@hits};  
+  
+  try{
+    my($offset,$limit,$total)=(0,200,0);
+    do{
+      
+      my $vpcore = mediamosa()->asset_job_list({
+        user_id => "Nara",
+        asset_id => $params->{asset_id},
+        limit => $limit,
+        offset => $offset
+      });
+      if($vpcore->header->request_result() ne "success"){        
+        confess $vpcore->header->request_result_description();
+      }
+      $total = $vpcore->header->item_count_total;
+      $vpcore->items->each(sub{
+        my $jobs = $_[0];
+        for my $id(keys %$jobs){
+          push @hits,$jobs->{$id};          
+        }
+      });
+
+      $offset += $limit;
+    }while($offset < $total);   
+  }catch{
+    push @errors,$_;
+  };
+
+  $response->{status} = scalar(@errors) == 0 ? "ok":"error";
+
+  content_type 'json';  
+  return to_json($response);
+
+};
+sub mediamosa {
+  state $mediamosa = MediaMosa->new(
+     %{ config->{mediamosa}->{rest_api} }
+  );
+}
+
+true;
